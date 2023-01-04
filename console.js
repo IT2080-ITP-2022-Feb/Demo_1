d0ee0c28991327010b0cd5ccae8e2c893ceb7c0c
Lahiru Lakshan / IT20664862 <75250622+Lahiru-weerasinghe@users.noreply.github.com>
2023-01-04 01:28:56 -0800

    Update console.js

--- console.js
+++ console.js
@@ -15,3 +15,21 @@ router.put('/Res/update/:id',(req,res)=>{
         }
     );
 });
+
+router.put('/Res/update/:id',(req,res)=>{
+    PostsRes.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set:req.body             
+        },
+        (err,reservation)=>{                       
+            if(err){
+                return res.status(400).json({error:err});
+            }
+
+            return res.status(200).json({
+                success:"Updat"
+            });
+        }
+    );
+});
